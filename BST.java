4132ee4cfe1ba6b2f840defafe116dffeae4e9ee
rbose365 <rishavbose365@gmail.com>
2015-09-17 00:17:41 -0400

    Changes

--- BST.java
+++ BST.java
@@ -1,6 +1,8 @@
 import java.util.Collection;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class BST<T extends Comparable<? super T>> implements BSTInterface<T> {
     // DO NOT ADD OR MODIFY INSTANCE VARIABLES.
@@ -42,19 +44,61 @@ public class BST<T extends Comparable<? super T>> implements BSTInterface<T> {
             root = addNode;
         } else {
             BSTNode<T> prevNode = this.searchPreviousNode(root, data);
-            if (prevNode.getData().equals(data)) {
-                return;
-            } else if (data.compareTo(prevNode.getData()) > 0) {
+//            if (prevNode.getData().equals(data)) {
+//                return;
+            if (data.compareTo(prevNode.getData()) > 0) {
                 prevNode.setRight(addNode);
-            } else if (data.compareTo(prevNode.getData()) > 0) {
+            } else if (data.compareTo(prevNode.getData()) < 0) {
                 prevNode.setLeft(addNode);
             }
         }
+        size++;
     }
 
     @Override
     public T remove(T data) {
-
+        if (data == null) {
+            throw new IllegalArgumentException("Data element is null");
+        }
+        BSTNode<T> prevNode = this.searchPreviousNode(root, data);
+        if (prevNode == null) {
+            throw new NoSuchElementException(data + " not found");
+        }
+        if (prevNode.getLeft().getData().equals(data)) {
+            BSTNode<T> replaceNode = prevNode.getLeft();
+            if (replaceNode.getRight() == null && replaceNode.getLeft() == null) {
+                prevNode.setLeft(null);
+            } else if(replaceNode.getRight() != null && replaceNode.getLeft() != null) {
+                prevNode.setLeft(replaceNode.getLeft());
+                prevNode.getLeft().setRight(replaceNode.getRight());
+            } else if (replaceNode.getRight() != null && replaceNode.getLeft() == null) {
+                prevNode.setLeft(replaceNode.getRight());
+            } else if (replaceNode.getLeft() != null && replaceNode.getRight() == null) {
+                prevNode.setLeft(replaceNode.getLeft());
+            } else {
+                throw new NoSuchElementException(data + " not found");
+            }
+            size--;
+            return replaceNode.getData();
+        } else if (prevNode.getRight().getData().equals(data)) {
+            BSTNode<T> replaceNode = prevNode.getRight();
+            if (replaceNode.getRight() == null && replaceNode.getLeft() == null) {
+                prevNode.setRight(null);
+            } else if(replaceNode.getRight() != null && replaceNode.getLeft() != null) {
+                prevNode.setRight(replaceNode.getLeft());
+                prevNode.getRight().setRight(replaceNode.getRight());
+            } else if (replaceNode.getRight() != null && replaceNode.getLeft() == null) {
+                prevNode.setRight(replaceNode.getRight());
+            } else if (replaceNode.getLeft() != null && replaceNode.getRight() == null) {
+                prevNode.setRight(replaceNode.getLeft());
+            } else {
+                throw new NoSuchElementException(data + " not found");
+            }
+            size--;
+            return replaceNode.getData();
+        } else {
+            throw new NoSuchElementException(data + " not found");
+        }
     }
 
     @Override
@@ -87,22 +131,28 @@ public class BST<T extends Comparable<? super T>> implements BSTInterface<T> {
 
     @Override
     public List<T> preorder() {
-
+        return null;
     }
 
     @Override
     public List<T> postorder() {
-
+        return null;
     }
 
     @Override
     public List<T> inorder() {
-
+        return null;
     }
 
     @Override
     public List<T> levelorder() {
-
+        Queue<BSTNode<T>> tempQueue = new LinkedList<BSTNode<T>>();
+        List<T> returnList = new List<T>();
+        tempQueue.add(root);
+        while (tempQueue.size() != 0) {
+            BSTNode<T> tempNode = tempQueue.remove();
+        }
+        return null;
     }
 
     @Override
@@ -112,7 +162,7 @@ public class BST<T extends Comparable<? super T>> implements BSTInterface<T> {
 
     @Override
     public int height() {
-
+        return findMaxHeight(root);
     }
 
     /**
@@ -127,7 +177,7 @@ public class BST<T extends Comparable<? super T>> implements BSTInterface<T> {
     }
 
     private BSTNode<T> searchNode(BSTNode<T> node, T data) {
-        if (node.getData().equals(data) || node == null) {
+        if (node == null || node.getData().equals(data)) {
             return node;
         } else if (data.compareTo(node.getData()) > 0) {
             return searchNode(node.getRight(), data);
@@ -138,22 +188,35 @@ public class BST<T extends Comparable<? super T>> implements BSTInterface<T> {
     }
 
     private BSTNode<T> searchPreviousNode(BSTNode<T> node, T data) {
-        if (node.getData().equals(data) || node == null) {
+        if (node == null || node.getData().equals(data)) {
             return node;
         } else if (data.compareTo(node.getData()) > 0) {
-            if (node.getRight() != null) {
-                return searchNode(node.getRight(), data);
-            } else {
+            if (node.getRight() == null || node.getRight().getData().equals(data)) {
                 return node;
+            } else {
+                return searchPreviousNode(node.getRight(), data);
             }
         } else if (data.compareTo(node.getData()) < 0) {
-            if (node.getLeft() != null) {
-                return searchNode(node.getLeft(), data);
-            } else {
+            if (node.getLeft() == null || node.getLeft().getData().equals(data)) {
                 return node;
+            } else {
+                return searchPreviousNode(node.getLeft(), data);
             }
         }
         return null;
     }
 
+    private int findMaxHeight(BSTNode<T> node) {
+        if (node == null) {
+            return -1;
+        }
+        int leftHeight = findMaxHeight(node.getLeft());
+        int rightHeight = findMaxHeight(node.getRight());
+
+        if (leftHeight > rightHeight) {
+            return leftHeight + 1;
+        } else {
+            return rightHeight + 1;
+        }
+    }
 }
